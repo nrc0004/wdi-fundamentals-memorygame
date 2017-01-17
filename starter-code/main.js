545ef50846eff2b9b9b73a63431669573c01bc4f
Natasha Clark <Natasharclark@gmail.com>
2017-01-16 23:43:29 -0500

    board created in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,26 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
 if (cardOne===cardTwo) {
 alert('You found a match!');
-} else if (cardThree===cardFour) {
-  alert('You found a match!');
+} 
+else if (cardThree===cardFour) {
+alert('You found a match!');
 } 
 else {
   alert('Sorry, try again.')
+} */
+
+var game-board = document.getElementById('game-board');
+function createBoard() {
+	for (var i=0; i<cards.length; i++){
+		var cardElement= docment.createElement('div');
+		cardElement.className= 'card';
+		board.appendChild(cardElement);
+	}
 }
+
+
